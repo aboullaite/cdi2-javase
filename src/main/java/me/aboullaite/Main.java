e88c8974528e76c9d55d866a0ebc06efdae4c89e
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2017-12-29 21:43:48 +0000

    Add maven plugins

--- src/main/java/me/aboullaite/Main.java
+++ src/main/java/me/aboullaite/Main.java
@@ -6,7 +6,7 @@ import javax.enterprise.inject.se.SeContainerInitializer;
 public class Main {
     public static void main(String[] args) {
         SeContainerInitializer initializer = SeContainerInitializer.newInstance();
-        /** disable discovery and register bean classes manually */
+        /** disable discovery and register classes manually */
         try (SeContainer container = initializer.disableDiscovery().addPackages(Main.class).initialize()) {
             container.select(Server.class);
         }
